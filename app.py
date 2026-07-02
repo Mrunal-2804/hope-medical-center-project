fde4574139b262b4f983f9ccfa3c928900dc4c46
Mrunal-2804 <mrunalpatil400@gmail.com>
2026-07-02 18:46:15 +0530

    Updated Hope Medical Center website

--- app.py
+++ app.py
@@ -286,5 +286,4 @@ def login():
 
 
 if __name__ == '__main__':
-    # Runs the server on http://127.0.0.1:5000
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
